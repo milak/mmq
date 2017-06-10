664318dd445d32ed3e7c846f39a74468051585d4
milak <milak@milak>
2017-06-10 19:06:43 +0200

    Update of document format

--- env/doc.go
+++ env/doc.go
@@ -1,7 +1,7 @@
-// The environnement passed to every service of mmq system
+// The environnement passed to every service of mmq system//
 // It contains : 
-// * the configuration
-// * MMQ system informations (Host, Name, etc.)
-// * The logger
-// * the status (running) of the MQ system
+//   * the configuration
+//   * MMQ system informations (Host, Name, etc.)
+//   * The logger
+//   * the status (running) of the MQ system
 package env
