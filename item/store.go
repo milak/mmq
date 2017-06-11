741653f0fe15c3a6d584f760dbcb6cf339031605
milak <milak@milak>
2017-06-11 18:28:06 +0200

    BUGFIX : add nil pointer check

--- item/store.go
+++ item/store.go
@@ -188,7 +188,7 @@ func (this *ItemStore) Pop(aTopicName string) (*Item, io.Reader, error) {
 					return nil, nil, StoreError{"Topic not found", subTopicName, "nil"}
 				}
 				items := this.itemsByTopic[subTopicName]
-				if !items.IsEmpty() {
+				if items != nil && !items.IsEmpty() {
 					item := items.PopHead()
 					event.Bus.FireEvent(&ItemRemoved{item, subTopic})
 					content, _ := this.GetContent(item.ID, true)
